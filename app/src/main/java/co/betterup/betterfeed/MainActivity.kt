844f6207f7cd0d10ba463b3c99b0d62653f6b9ca
Nick Donaldson <ndonald2@gmail.com>
2019-11-18 14:39:00 -0700

    Initial commit

---
+++ app/src/main/java/co/betterup/betterfeed/MainActivity.kt
@@ -0,0 +1,17 @@
+package co.betterup.betterfeed
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * This is just provided as a starting point and this and any other part of this project can
+ * be changed in any way you see fit.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
